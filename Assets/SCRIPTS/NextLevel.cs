29f40529af437bc6f40505456ad0c737c6c78752
Beñat <zabalaortizbezo@gmail.com>
2026-02-25 00:42:57 +0100

    Niveles terminados y funcionales

---
+++ Assets/SCRIPTS/NextLevel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public string nextLevel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Nivel2");
+        }
+    }
+}
+
